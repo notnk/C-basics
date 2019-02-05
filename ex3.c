1d7030c8387c80c74ea054968ec0860dc1d55402
Navneeth Krishna <34378434+notnk@users.noreply.github.com>
2019-02-05 14:32:33 +0530

    Add files via upload

---
+++ ex3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	float r,c,a;
+	printf("Enter the radius of thr circle:");
+	scanf("%f",&r);
+	a=3.14*r*r;
+	c=2*3.14*r;
+	printf("The area is:%f",a,"\nThe cir is %f",c);
+}
