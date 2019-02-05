b135641b808133c35b982737a09bb1c64b90ab31
Navneeth Krishna <34378434+notnk@users.noreply.github.com>
2019-02-05 14:32:01 +0530

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	float b,c;
+	printf("Enter the floating point:");
+	scanf("%f",&b);
+	a=b;
+	c=b-a;
+	printf("This is the integer point%d",a);
+	printf("\nThis is the floating point.%f",c);
+} 
