85686c835f2cd29982df9d30ab86c198b31d4e06
Navneeth Krishna <34378434+notnk@users.noreply.github.com>
2019-03-29 09:12:57 +0530

    Add files via upload

---
+++ stud.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct stud
+{
+	char name[100];
+	int m[5];
+  	float avg[100];
+}s[100];
+void main()
+{	
+	int t[100],n;
+	printf("Enter the total no of students:");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		printf("Enter the name of the %dth student:",i+1);
+		scanf("%s",s[i].name);
+		printf("Enter the 5 marks of the %dth student:",i+1);
+		for(int j=0;j<5;j++)
+		{
+			scanf("%d",&s[i].m[j]);
+			t[i]+=s[i].m[j];
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		s[i].avg[i]=t[i]/5;
+		printf("%s\t%d\t%d\t%d\t%d\t%d\t%f\n",s[i].name,s[i].m[0],s[i].m[1],s[i].m[2],s[i].m[3],s[i].m[4],s[i].avg[i]);
+	}
+}
