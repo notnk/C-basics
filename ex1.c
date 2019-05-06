8af4d2cc665382f0e40bcd3983bd8434635fc111
Navneeth Krishna <34378434+notnk@users.noreply.github.com>
2019-05-06 22:18:54 +0530

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -1,12 +1,44 @@
 #include<stdio.h>
+struct date
+{
+	int d,m,y;
+}t;
+int check(int d,int m,int y)
+{
+	if(m>12)	
+		return -1;
+	if(y<1000 && y>10000)
+		return -1;
+	if(d>31)
+		return -1;
+	if(m==2)
+		if(y%400==0 && m<30)
+			return 0;
+		else 
+			return -1;
+	if( y%400==0)
+		if(m==2 && d>29)
+			return -1;
+	else
+	{
+		if(d==31)
+		{
+			if(m==2 || m==4 || m==7 || m==8 || m== 10 )
+			{
+				return -1;
+			}
+		}	
+	}
+	return 0;
+}
 void main()
 {
-	int a;
-	float b,c;
-	printf("Enter the floating point:");
-	scanf("%f",&b);
-	a=b;
-	c=b-a;
-	printf("This is the integer point%d",a);
-	printf("\nThis is the floating point.%f",c);
-} 
+	scanf("%d%d%d",&t.d,&t.m,&t.y);
+	int x=check(t.d,t.m,t.y);
+	if(x==0)
+	{
+		printf("the date is valid");
+	}
+	else
+		printf("The date is not valid");
+}
